26451ae1a53bfb4e58d928541f2f1e41bac2850f
Ricardo Negrete Ruiz <ricardo@projetamp.io>
2023-01-20 17:18:20 -0600

    Added dependency injection

---
+++ src/api/Configuration/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Neltic.Shared.Api.Configuration;
+public static class DependencyInjection
+{
+    public static IServiceCollection InstallServices(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        params Assembly[] assemblies)
+    {
+        IEnumerable<IServiceInstaller> serviceInstallers = assemblies
+            .SelectMany(a => a.DefinedTypes)
+            .Where(IsAssignableToType<IServiceInstaller>)
+            .Select(Activator.CreateInstance)
+            .Cast<IServiceInstaller>();
+
+        foreach (IServiceInstaller serviceInstaller in serviceInstallers)
+        {
+            serviceInstaller.Install(services, configuration);
+        }
+
+        return services;
+
+        static bool IsAssignableToType<T>(TypeInfo typeInfo) =>
+            typeof(T).IsAssignableFrom(typeInfo) &&
+            !typeInfo.IsInterface &&
+            !typeInfo.IsAbstract;
+    }
+}
---
+++ src/api/Configuration/IServiceInstaller.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Neltic.Shared.Api.Configuration;
+public interface IServiceInstaller
+{
+    void Install(IServiceCollection services, IConfiguration configuration);
+}

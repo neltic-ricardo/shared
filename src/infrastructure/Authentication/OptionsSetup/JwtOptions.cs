58c5f2c6200f9608599d3cc44c83927b9aee6b8d
Ricardo Negrete Ruiz <ricardo@projetamp.io>
2023-01-27 17:18:49 -0600

    Added access Expiration and refresh expiration on jwtoptions

--- src/infrastructure/Authentication/OptionsSetup/JwtOptions.cs
+++ src/infrastructure/Authentication/OptionsSetup/JwtOptions.cs
@@ -6,4 +6,6 @@ public class JwtOptions
     public string SecretKey { get; set; } = string.Empty;
     public string Issuer { get; set; } = string.Empty;
     public string Audience { get; set; } = string.Empty;
+    public int AccessTokenExpiration { get; set; }
+    public int RefreshTokenExpiration { get; set; }
 }

5d494576284c3388f52b2193c0855266a604be2b
Ricardo Negrete Ruiz <ricardo@projetamp.io>
2023-01-31 13:39:33 -0600

    WIP

---
+++ src/application/ApplicationAssembly.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace Neltic.Identity.Application;
+
+public static class ApplicationAssembly
+{
+    public static readonly Assembly Instance = typeof(ApplicationAssembly).Assembly;
+}
---
+++ src/infrastructure/InfrastructureAssembly.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+namespace Neltic.Identity.Infrastructure;
+public static class InfrastructureAssembly
+{
+    public static readonly Assembly Instance = typeof(InfrastructureAssembly).Assembly;
+}
